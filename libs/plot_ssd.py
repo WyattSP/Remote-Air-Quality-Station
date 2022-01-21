dc430438b5c249cdffa69a6f01549fb5277d68fd
Wyatt <69209835+WyattSP@users.noreply.github.com>
2022-01-21 09:35:58 -0700

    Update plot_ssd.py

--- libs/plot_ssd.py
+++ libs/plot_ssd.py
@@ -13,7 +13,7 @@ path = r'Path'
 data = pd.read_csv(path+'aq_log.txt', sep=",", header=None)
 
 #Set column names
-data.columns = ["Time", "PM2.5", "AQI 2.5", "PM10", "AQI PM 10"]
+data.columns = ["Time", "PM 2.5", "AQI 2.5", "PM 10", "AQI 10"]
 
 #Plot data
 fig = px.line(data, x="Time", y=data.columns,
